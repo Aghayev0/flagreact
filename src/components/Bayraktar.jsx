7738b4723821b8f4678821cb3fc16dba3740a4b8
Aghayev0 <tu6k6wzh2@code.edu.az>
2022-12-26 05:12:52 +0400

    muellim alindi

--- src/components/Bayraktar.jsx
+++ src/components/Bayraktar.jsx
@@ -3,16 +3,36 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function Flags() {
-    const [bayraq, setBayraq] = useState([])
+    const [flags, setFlags] = useState([])
     const { cca2 } = useParams()
+    const [isLoading, setLoading] = useState(true);
     useEffect(() => {
         axios
             .get(`https://restcountries.com/v3.1/alpha/${cca2}`)
-            .then((res) => setBayraq(res.data))
-    }, [])
-    return (
-        console.log(bayraq[0])
-    )
+            .then((res) => {
+                setFlags(res.data);
+                setLoading(false);
+
+            }, 3000);
+    }, []);
+    if (isLoading) {
+        return (
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100vh",
+            }}>Loading the data {console.log("loading state")}</div>
+        );
+    }
+    return (<>
+        <p>{flags[0].area}</p>
+        <p>{flags[0].name.common}</p>
+        <p>{flags[0].capital}</p>
+        <p>{flags[0].region}</p>
+    </>
+    );
 }
 
 export default Flags
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,12 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Kart from './Kart'
+import React from 'react'
+import Ev from './Ev'
 
 function Home() {
-    const [flags, setFlags] = useState([])
-    useEffect(() => {
-        axios.get("https://restcountries.com/v3.1/all").then(res => setFlags(res.data))
-    })
     return (
-        flags.map((flag) => {
-            return (
-                <Link key={flag.cca2} to={`/flag/${flag.cca2}`}>
-                    <Kart  flag={flag} />
-                </Link>
-            )
-        })
+        <>
+            <Ev/>
+        </>
     )
 }
+
 export default Home
